4e66a0ff7870ce856f1296aa1170bba4ff73477e
dzpromark <markart056@gmail.com>
2024-07-15 03:06:19 +0800

    Fixed React Hook and linting issues

--- app/components/Clipa.jsx
+++ app/components/Clipa.jsx
@@ -1,41 +1,44 @@
 "use client"
-import React, { useEffect } from 'react'
-import { useScroll } from 'framer-motion'
-import { useRef } from 'react'
+import React, { useEffect, useRef } from 'react';
+import { useScroll } from 'framer-motion';
+
 function Clipa() {
     const container = useRef();
-    const texts = useRef([])
-    const {scrollYProgress} =  useScroll({
+    const texts = useRef([]);
+    const { scrollYProgress } = useScroll({
         target: container,
         offset: ['start end', 'end end']
-    })
-    useEffect(()=>{
-        scrollYProgress.on('change', e=>{
-            texts.current.forEach((text,i)=>{
-                text.setAttribute('startOffset', -40 + (i * 40) + (e * 40) + "%")
-            })
-        })
-    },[])
+    });
 
-  return (
-    <div className='border-b border-teal-900'>
-        <h1 className='text-white text-center text-3xl py-10 lg:text-5xl'>Hi!</h1>
-        <h1 className='text-white text-center text-3xl py-4 lg:text-5xl'>I'm</h1>
+    useEffect(() => {
+        const unsubscribe = scrollYProgress.on('change', (e) => {
+            texts.current.forEach((text, i) => {
+                text.setAttribute('startOffset', -40 + (i * 40) + (e * 40) + "%");
+            });
+        });
 
-        <svg className='mb-40' viewBox='0 0 250 90'>
-            <path id='curve'  fill="none" d="m0,88.5c61.37,0,61.5-68,126.5-68,58,0,51,68,123,68" />
-             <text fill='white' className='text-[6px]' style={{color:"red"}}>
-                {
-                    [...Array(3)].map((_, i)=>{
-                        return <textPath key={i} ref={ref=> texts.current[i] = ref} href='#curve' startOffset={i * 40 + "%"}>FullStack Developer: Jhon Mark Vergara</textPath>
+        return () => {
+            unsubscribe();
+        };
+    }, [scrollYProgress]);
 
-                    })
-                }
-             </text>
-        </svg>
-       
-    </div>
-  )
+    return (
+        <div className='border-b border-teal-900' ref={container}>
+            <h1 className='text-white text-center text-3xl py-10 lg:text-5xl'>Hi!</h1>
+            <h1 className='text-white text-center text-3xl py-4 lg:text-5xl'>I&apos;m</h1>
+
+            <svg className='mb-40' viewBox='0 0 250 90'>
+                <path id='curve' fill="none" d="m0,88.5c61.37,0,61.5-68,126.5-68,58,0,51,68,123,68" />
+                <text fill='white' className='text-[6px]' style={{ color: "red" }}>
+                    {
+                        [...Array(3)].map((_, i) => {
+                            return <textPath key={i} ref={ref => texts.current[i] = ref} href='#curve' startOffset={i * 40 + "%"}>FullStack Developer: Jhon Mark Vergara</textPath>
+                        })
+                    }
+                </text>
+            </svg>
+        </div>
+    );
 }
 
-export default Clipa
+export default Clipa;
--- app/components/Contact.jsx
+++ app/components/Contact.jsx
@@ -1,6 +1,6 @@
-import React from 'react'
-import { CONTACT } from '../../constants'
-import { motion } from "framer-motion"
+import React from 'react';
+import { CONTACT } from '../../constants';
+import { motion } from "framer-motion";
 
 const Contact = () => {
   return (
@@ -10,23 +10,17 @@ const Contact = () => {
         </h2>
         <div className="text-center tracking-tighter">
         <p className='my-4 text-2xl text-red-600'>This Portfolio Website is not yet Done due to my computer is not Functioning well!</p>          
-            <p
-    
-            className='my-4 text-white'>{CONTACT.address}</p>
-            <p 
-    
-            className="my-4 text-white">{CONTACT.phoneNo}</p>
-            <a 
-             
-            href="#" className='text-white'>{CONTACT.email}</a>
+            <p className='my-4 text-white'>{CONTACT.address}</p>
+            <p className="my-4 text-white">{CONTACT.phoneNo}</p>
+            <a href="#" className='text-white'>{CONTACT.email}</a>
 
             <p className='text-white pt-10'>
-            © 2024 Vergara's Corporation. All Rights Reserved.
+            © 2024 Vergara&apos;s Corporation. All Rights Reserved.
             </p>
         </div>
         
     </div>
-  )
+  );
 }
 
-export default Contact
+export default Contact;
